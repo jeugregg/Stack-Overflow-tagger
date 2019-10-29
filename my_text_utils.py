73e97c61446eade25a49868634903124929a3618
jeugregg <jeugregg@hotmail.com>
2019-10-29 18:53:59 +0100

    API integration : export modl & script to be executed on server.
    fixes #14
    fixes #15
    fixes #16

---
+++ my_text_utils.py
@@ -0,0 +1,27 @@
+# -*-coding:utf-8 -*
+import nltk
+from nltk.stem.snowball import EnglishStemmer
+import re
+
+def myTokenizer(text):
+    '''
+    Create tokens from text
+    '''
+    def stem_tokens(tokens, stemmer):
+        '''
+        Stem words in tokens.
+        and suppress word < 3 characters
+        '''
+        stemmed = []
+        for item in tokens:
+            if re.match('[a-zA-Z0-9]{3,}',item):
+                stemmed.append(stemmer.stem(item))
+        return stemmed
+
+    tokens = nltk.word_tokenize(text)
+    stems = stem_tokens(tokens, EnglishStemmer())
+    return stems
+
+# test de la fonction myTokenizer
+if __name__ == "__main__":
+    print(myTokenizer("e 123 e the module is firebase"))
